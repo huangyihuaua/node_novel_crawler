1787ac932b97688dc824ffdc6640e670288df6a4
huangyihua <779447842@qq.com>
2018-10-16 15:47:58 +0800

    加请求头

--- crawler.js
+++ crawler.js
@@ -4,7 +4,12 @@ module.exports = (url,method,cb)=>{
     url,
     method,
     headers: {
-      "User-Agent": "request",
+      'Accept': '*/*',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Cache-Control': 'max-age=0',
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+      'Connection': 'keep-alive',
+      'Referer': 'http://www.baidu.com/'
     },
     rejectUnauthorized: false
   };
